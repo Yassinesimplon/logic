d5342645519ae004e09c5b60fecc74ac6efa824f
Yassine <hasnacine@gmail.com>
2022-12-11 08:50:12 +0100

    logic

---
+++ logique.py
@@ -0,0 +1,14 @@
+clim=input('rainy?')
+
+pow=input("power ?")
+if clim=="yes" and pow=="yes":
+  print("netflix")
+elif clim=="yes" and pow=="no":
+  print("read ktab")
+
+hot=input("to hoot ?")
+if clim=="no" and hot=="yes":
+  print("visit mall")
+elif clim=="no" and hot=="no":
+  print("visit garden")
+
